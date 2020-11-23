e3a7180b24a22dd952e830bee089f36ffbc6c4c4
juegonjue <tojue123@naver.com>
2020-11-24 02:23:12 +0900

    Updates

---
+++ precache-manifest.8959dd36986290d17763d003d2675794.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e895b9aee17a289ffd4bd4b5112c9da",
+    "url": "/fleamarket/client/index.html"
+  },
+  {
+    "revision": "bf14b0247460bd78d07f",
+    "url": "/fleamarket/client/static/css/main.a8886c39.chunk.css"
+  },
+  {
+    "revision": "9b2772b3f50277f69d63",
+    "url": "/fleamarket/client/static/js/2.2a35cb79.chunk.js"
+  },
+  {
+    "revision": "e75ee3023818c28e96dbd97832cf83bb",
+    "url": "/fleamarket/client/static/js/2.2a35cb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf14b0247460bd78d07f",
+    "url": "/fleamarket/client/static/js/main.0a3ce316.chunk.js"
+  },
+  {
+    "revision": "b74c931f8d85dbe6fdfc",
+    "url": "/fleamarket/client/static/js/runtime-main.9f520fe4.js"
+  },
+  {
+    "revision": "499536a0b83ed2b94ffc52c1d20bd9f0",
+    "url": "/fleamarket/client/static/media/profile.499536a0.png"
+  },
+  {
+    "revision": "6a33ea7bb63863d50405f0b048d0db92",
+    "url": "/fleamarket/client/static/media/school2.6a33ea7b.csv"
+  },
+  {
+    "revision": "aa426d6cfd00a571173ed9ffd39209a3",
+    "url": "/fleamarket/client/static/media/test23.aa426d6c.png"
+  },
+  {
+    "revision": "a80b931507e983fe7bb44db19e135690",
+    "url": "/fleamarket/client/static/media/test24.a80b9315.png"
+  }
+]);
